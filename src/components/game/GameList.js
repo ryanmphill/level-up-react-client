79dbefb69ded4380a3cb738de60dc3cc2882377a
Ryan Phillips <ryanmphill@gmail.com>
2023-08-09 09:57:05 -0500

    Listed games on home view

---
+++ src/components/game/GameList.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react"
+import { getGames } from "../../managers/GameManager"
+
+
+export const GameList = (props) => {
+    const [ games, setGames ] = useState([])
+
+    useEffect(() => {
+        getGames().then(data => setGames(data))
+    }, [])
+
+    return (
+        <article className="games">
+            {
+                games.map(game => {
+                    return <section key={`game--${game.id}`} className="game">
+                        <div className="game__title">{game.title} by {game.maker.full_name}</div>
+                        <div className="game__players">{game.number_of_players} players needed</div>
+                        <div className="game__skillLevel">Skill level is {game.skill_level}</div>
+                    </section>
+                })
+            }
+        </article>
+    )
+}
